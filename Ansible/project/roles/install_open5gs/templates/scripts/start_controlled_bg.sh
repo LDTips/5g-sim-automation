f46bc62352eec8a739ef98c60fcffd4d6f94ec3a
LDTips <batruchs@gmail.com>
2023-07-14 18:49:41 +0200

    Script progress and variable simplification

--- Ansible/project/roles/install_open5gs/templates/scripts/start_controlled_bg.sh
+++ Ansible/project/roles/install_open5gs/templates/scripts/start_controlled_bg.sh
@@ -1,18 +1,37 @@
 #!/bin/bash
 
-stop_child_processes() {
+stop_all_child_jobs() {
     echo ""
-    echo "Stopping jobs..."
+    echo "Stopping open5gs daemons..."
     pkill -P $$
-    echo "Jobs stopped"
+    echo "Open5gs jobs stopped"
 }
 
-trap stop_child_processes EXIT SIGTERM SIGINT
+trap stop_all_child_jobs EXIT SIGTERM SIGINT
 
 # Start background processes
-yes > /dev/null &
-yes > /dev/null &
+/bin/open5gs-nrfd > /dev/null &
+sleep 1
+/bin/open5gs-scpd > /dev/null &
+sleep 1
+/bin/open5gs-amfd > /dev/null &
+sleep 1
+/bin/open5gs-smfd > /dev/null &
+sleep 1
+/bin/open5gs-ausfd > /dev/null &
+sleep 1
+/bin/open5gs-udmd > /dev/null &
+sleep 1
+/bin/open5gs-udrd > /dev/null &
+sleep 1
+/bin/open5gs-pcfd > /dev/null &
+sleep 1
+/bin/open5gs-nssfd > /dev/null &
+sleep 1
+/bin/open5gs-bsfd > /dev/null &
+sleep 1
+/bin/open5gs-upfd > /dev/null &
 
-echo "Started scripts..."
+echo "Simulator daemons started. Send termination signal to end..."
 
 read -r -d '' _ < /dev/tty  # 'Locks' the script waiting for input

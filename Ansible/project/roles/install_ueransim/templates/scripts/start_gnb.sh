43fb9f6b8e88f366b49b657f93c934eb5b69a238
LDTips <batruchs@gmail.com>
2023-07-24 21:38:51 +0200

    Minor fix

--- Ansible/project/roles/install_ueransim/templates/scripts/start_gnb.sh
+++ Ansible/project/roles/install_ueransim/templates/scripts/start_gnb.sh
@@ -3,7 +3,7 @@
 # Start gnb process
 # It is required to add -c <config path> to specify the config
 # e.g. screen -dm ./home/open5gs-ansible/UERANSIM/nr-gnb -c /path/to/some/gnb/config.yaml
-screen -dm ./home/open5gs-ansible/UERANSIM/nr-gnb -c # INSERT CONFIG PATH HERE
+screen -dm /home/open5gs-ansible/UERANSIM/build/nr-gnb -c # INSERT CONFIG PATH HERE
 
 if [[ $EUID -ne 0 ]]; then
   echo "User who run the script was not root!"
